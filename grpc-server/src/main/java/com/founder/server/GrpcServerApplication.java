68babdc4af3a803bb4e17f4a5b323ff688e40b78
huangguochao <917929050@qq.com>
2019-08-22 09:21:42 +0800

    <feat> 调试Go调用Java服务

---
+++ grpc-server/src/main/java/com/founder/server/GrpcServerApplication.java
@@ -0,0 +1,13 @@
+package com.founder.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GrpcServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcServerApplication.class, args);
+    }
+}
---
+++ grpc-server/src/main/java/com/founder/server/grpc/GrpcServerService.java
@@ -0,0 +1,18 @@
+package com.founder.server.grpc;
+
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class GrpcServerService extends GreeterGrpc.GreeterImplBase {
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello====>" + request.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
